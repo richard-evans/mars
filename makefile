600aca6064fb35dc50cda48c9c60eda8e2b1f2fe
Richard Evans <richard.evans@york.ac.uk>
2014-02-04 21:28:23 +0000

    Added main function and elementary makefile

---
+++ makefile
@@ -0,0 +1,45 @@
+#----------------------------------------------------------------------------
+#
+#                       Makefile for ARM
+#
+#----------------------------------------------------------------------------
+
+# Compiler (c++11 support required)
+GCC=g++-mp-4.7
+
+# LIBS
+LIBS=-lstdc++
+
+# Performance Flags
+GCC_CFLAGS= -std=c++11 -O3 -falign-labels -falign-loops -funroll-all-loops -fexpensive-optimizations -funroll-loops -I./hdr
+GCC_LDFLAGS= -std=c++11 -lstdc++ -I./hdr
+
+# Objects
+OBJECTS= \
+obj/main/main.o
+
+EXECUTABLE=arm
+
+all: $(OBJECTS) serial
+
+# Serial Targets
+serial: $(OBJECTS)
+	$(GCC) $(GCC_LDFLAGS) $(LIBS) $(OBJECTS) -o $(EXECUTABLE)
+
+$(OBJECTS): obj/%.o: src/%.cpp
+	$(GCC) -c -o $@ $(GCC_CFLAGS) $<
+
+clean:
+	@rm -f obj/*.o
+	@rm -f obj/*/*.o
+
+purge:
+	@rm -f obj/*.o
+	@rm -f obj/*/*.o
+	@rm -f arm
+
+tidy:
+	@rm -f *~
+	@rm -f hdr/*~
+	@rm -f src/*~
+	@rm -f src/*/*~
---
+++ src/main/main.cpp
@@ -0,0 +1,47 @@
+//-----------------------------------------------------------------------------
+//
+// This source file is part of the ARM open source package under the
+// BSD (2-clause) licence (see LICENCE file for details).
+//
+// (c) R F L Evans 2014. All rights reserved.
+//
+//-----------------------------------------------------------------------------
+
+// System headers
+#include <iostream>
+
+// Program headers
+
+
+int main(){
+
+   // Output Program Header
+   //----------------------------------------------------------------------------------------------------------
+   std::cout << std::endl;
+   std::cout << "                                  ,---.,---.,-.-." << std::endl;
+   std::cout << "                                  ,---||    | | |" << std::endl;
+   std::cout << "                                  `---^`    ` ' '" << std::endl;
+   std::cout << std::endl;
+   std::cout << "                      Version 1.0.0 " << __DATE__ << " " << __TIME__ << std::endl;
+   std::cout << std::endl;
+
+   std::cout << "  Licensed under the BSD (2-Clause) License. See licence file for details." << std::endl;
+   std::cout << std::endl;
+   std::cout << "  Lead Developer: Richard F L Evans" << std::endl;
+   std::cout << std::endl;
+   std::cout << "  Contributors:" << std::endl;
+   std::cout << std::endl;
+   std::cout << "--------------------------------------------------------------------------------" << std::endl;
+
+   // Initialise system
+   //arm::initialise(infile);
+
+   // Create system
+   //arm::create();
+
+   // Simulate system
+   //arm::simulate();
+
+   return 0;
+
+}
