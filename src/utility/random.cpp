3f4c3a6d46ca68872b485b845d58f0023134f619
Richard Evans <richard.evans@york.ac.uk>
2014-02-12 11:36:53 +0000

    Added implementation of basic rng

---
+++ src/utility/random.cpp
@@ -0,0 +1,25 @@
+//-----------------------------------------------------------------------------
+//
+// This source file is part of the ARM open source package under the
+// BSD (2-clause) licence (see LICENCE file for details).
+//
+// (c) R F L Evans 2014. All rights reserved.
+//
+//-----------------------------------------------------------------------------
+
+// System headers
+#include <functional>
+#include <random>
+
+// Program headers
+
+namespace arm{
+
+   // Integrator random numbers
+   std::mt19937 eng(12345); // This is the Mersenne Twister
+   // Gaussian (normal) distribution, mean = 0, sigma=1.0
+   std::normal_distribution<double> dist(0.0,1.0);
+   // Create bound auto object for rng, get new numbers using irnd()
+   auto irnd = std::bind (dist,eng);
+
+}
