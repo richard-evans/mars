2b0743f0b4150765bcf185d034f9ecced3c940b3
Richard Evans <richard.evans@york.ac.uk>
2015-03-03 22:16:04 +0000

    Added new parameters for create module

--- src/create/interface.cpp
+++ src/create/interface.cpp
@@ -26,31 +26,74 @@ namespace mars{
       // create variables
       //grain_structure_t grain_structure; // type of structure to be generated
 
-      //double system_size_x; // nm
-      //double system_size_y;
-    //double system_size_z;
-
     //double grain_size; //nm
     //  double grain_spacing; //nm
-
-      // set up input file parameters
-      //mars::interface::parameters::add_int_parameter("x-pixels", NONE, 1, 1000000, a);
 
       //  |-----------------------------80 characters wide------------------------------|
       {const char *manual =
           "\n"
 	  " Syntax:\n\n"
-          "    system-size-x = <x> !<unit>\n\n"
+          "    create:system-size-x = <x> [!<unit>]\n\n"
           " system-size-x specifies the size of the system along the x coordinate\n"
           " direction. The system size can be specified between 1 nanometre and\n"
           " 100 micrometres.\n\n"
 	  " The default units are nanometres, but may be set using the optional unit\n"
 	  " character (!). Valid units include nanometres (nm), micrometres (um),\n"
 	  " metres (m) and inches (in).\n\n";
-	mars::interface::parameters.add_new_double_parameter("system-size-x", libinput::NONE, 1, 100000, mars::create::internal::system_size_x, manual);}
+	mars::interface::parameters.add_new_double_parameter("create:system-size-x", libinput::NONE, 1, 100000, mars::create::internal::system_size_x, manual);}
+
+      //  |-----------------------------80 characters wide------------------------------|
+      {const char *manual =
+	  "\n"
+	  " Syntax:\n\n"
+	  "    create:system-size-y = <y> [!<unit>]\n\n"
+	  " system-size-y specifies the size of the system along the y coordinate\n"
+	  " direction. The system size can be specified between 1 nanometre and\n"
+	  " 100 micrometres.\n\n"
+	  " The default units are nanometres, but may be set using the optional unit\n"
+	  " character (!). Valid units include nanometres (nm), micrometres (um),\n"
+	  " metres (m) and inches (in).\n\n";
+	mars::interface::parameters.add_new_double_parameter("create:system-size-y", libinput::NONE, 1, 100000, mars::create::internal::system_size_y, manual);}
+
+      //  |-----------------------------80 characters wide------------------------------|
+      {const char *manual =
+	  "\n"
+	  " Syntax:\n\n"
+	  "    create:system-size-z = <z> [!<unit>]\n\n"
+	  " system-size-z specifies the size of the system along the z coordinate\n"
+	  " direction. The system size can be specified between 1 nanometre and\n"
+	  " 100 micrometres.\n\n"
+	  " The default units are nanometres, but may be set using the optional unit\n"
+	  " character (!). Valid units include nanometres (nm), micrometres (um),\n"
+	  " metres (m) and inches (in).\n\n";
+	mars::interface::parameters.add_new_double_parameter("create:system-size-z", libinput::NONE, 1, 100000, mars::create::internal::system_size_z, manual);}
+
+      //  |-----------------------------80 characters wide------------------------------|
+      {const char *manual =
+	  "\n"
+	  " Syntax:\n\n"
+	  "    create:grain-size = <a> [!<unit>]\n\n"
+	  " create:grain-size specifies the lateral size of each grain in the system.\n"
+	  " direction. The grain size can be specified between 1 and 100 nanometres.\n\n"
+	  " The default units are nanometres, but may be set using the optional unit\n"
+	  " character (!). Valid units include nanometres (nm), micrometres (um),\n"
+	  " metres (m) and inches (in).\n\n";
+	mars::interface::parameters.add_new_double_parameter("create:grain-size", libinput::NONE, 1, 100, mars::create::internal::grain_size, manual);}
+
+      //  |-----------------------------80 characters wide------------------------------|
+      {const char *manual =
+	  "\n"
+	  " Syntax:\n\n"
+	  "    create:grain-spacing = <a> [!<unit>]\n\n"
+	  " create:grain-spacing specifies the spacing between grains in the system.\n"
+	  " The grain spacing can be specified between 1 and 100 nanometres.\n\n"
+	  " The default units are nanometres, but may be set using the optional unit\n"
+	  " character (!). Valid units include nanometres (nm), micrometres (um),\n"
+	  " metres (m) and inches (in).\n\n";
+	mars::interface::parameters.add_new_double_parameter("create:grain-spacing", libinput::NONE, 1, 100, mars::create::internal::grain_spacing, manual);}
 
       //std::cout << "\033[32m" << "system-size-x" << "\033[0m" << std::endl;
-      //std::cout << mars::interface::parameters.get_manual("system-size-x") << std::endl;
+        //std::cout << mars::interface::parameters.get_manual("system-size-x") << std::endl;
 
       return; 
     }
